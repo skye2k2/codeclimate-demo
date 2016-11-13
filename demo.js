6ad876a6c19bf9ba9fc17c8741be082a32cce010
Clif Bergmann <cbergmann@familysearch.org>
2016-11-13 09:26:09 -0700

    Demo Code Climate eslint capabilities
    Create ONE_FUNCTION_TO_BRING_THEM_ALL_AND_IN_THE_DARKNESS_BIND_THEM function with as many eslint infractions as humanly possible.
    Add awesome styles for monochrome televisions.

---
+++ demo.js
@@ -0,0 +1,36 @@
+
+// FIXME: Actually make this work
+// TODO: Add documentation
+function ONE_FUNCTION_TO_BRING_THEM_ALL_AND_IN_THE_DARKNESS_BIND_THEM(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) {
+  debugger;// Make sure we get in here to set the value correctly
+
+  if (((a == a))) {} else if (b == b) {} else if (c == c) {} else if (d == d) {} else if (e == e) {} else if (f == f) {} else if (g == g) {} else if (h == h) {} else if (i == i) {} else if (j == null) {return} else {undefined = true;}
+
+  switch (a) {
+    case 1:
+      alert("I'm in a switch statement for variable a");
+      var x = undefined;
+    return null;
+  }
+
+  switch (b) {
+    case 1:
+      alert("I'm in a switch statement for variable b");
+      var x = x;
+    return null;
+  }
+
+  func = function(){}
+
+  // HACK: By newing a number, we get no additional benefit
+  var x = new Number(parseInt(1));
+  x.property = {a: x
+    ,}
+
+
+
+  delete x;
+  return true;
+  return false;
+}
+
