ed09715a2a0ce371e8134818eda07ece1f81ea40
Clif Bergmann <cbergmann@familysearch.org>
2016-11-14 07:30:23 -0700

    Remove/rename deprecated eslint rules
    Update ONE_FUNCTION_TO_BRING_THEM_ALL_AND_IN_THE_DARKNESS_BIND_THEM.

--- demo.js
+++ demo.js
@@ -4,20 +4,37 @@
 function ONE_FUNCTION_TO_BRING_THEM_ALL_AND_IN_THE_DARKNESS_BIND_THEM(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) {
   debugger;// Make sure we get in here to set the value correctly
 
-  if (((a == a))) {} else if (b == b) {} else if (c == c) {} else if (d == d) {} else if (e == e) {} else if (f == f) {} else if (g == g) {} else if (h == h) {} else if (i == i) {} else if (j == null) {return} else {undefined = true;}
+  if (((a == a))) {} else if (b) {} else if (c) {} else if (d) {} else if (e) {} else if (f) {} else if (g) {} else if (h) {} else if (i == "NaN") {} else if (j == null) {return} else {undefined = true;}
 
   switch (a) {
     case 1:
-      alert("I'm in a switch statement for variable a");
+      alert("I'm in a switch statement for when variable a is equal to one");
       var x = undefined;
+    return null;
+    case 1:
+      alert("I'm in a switch statement for when variable a is equal to one");
+      var x = x;
     return null;
   }
 
-  switch (b) {
-    case 1:
-      alert("I'm in a switch statement for variable b");
-      var x = x;
-    return null;
+  if (a != b) {
+    for (i = 0; i < a; i++) {
+      for (j = 0; j < b; j++) {
+        if (j === i * 2 + 1) {
+          console.log("Ash nazg durbatulûk, ash nazg gimbatul, Ash nazg thrakatulûk agh burzum-ishi krimpatul.");
+        }
+      }
+    }
+  }
+
+  if (c != d) {
+    for (i = 0; i < c; i++) {
+      for (j = 0; j < d; j++) {
+        if (j === i * 2 + 1) {
+          console.log("One ring to rule them all, one ring to find them, One ring to bring them all and in the darkness bind them.");
+        }
+      }
+    }
   }
 
   func = function(){}
